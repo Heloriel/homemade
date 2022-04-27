af71ab7a365b33e00ea9b90d14b838eec4e980e2
Heloriel <andradematheus.dev@gmail.com>
2022-04-27 11:35:41 -0300

    2022 Update 2.2

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -19,9 +19,9 @@ export default (props) => {
           <br />
           <p>Em 2013 tive a experiência de 1 ano em meu primeiro emprego como desenvolvedor full stack com php.</p>
           <br />
-          <p>Em 2015 comecei a trabalhar como Designer Gráfico por quase 7 anos e agora estou em processo de transição de volta ao que tenho paixão, técnologia e programação dentro da mesma empresa.</p>
+          <p>Em 2015 comecei a trabalhar como Designer Gráfico por quase 7 anos e agora sou Técnico em TI dentro da mesma empresa.</p>
           <br />
-          <p>Atualmente sou Técnico em TI e meu foco e minha paixão é por desenvolvimento front-end com JavaScript, React e seu vasto ecossistema.</p>
+          <p>Atualmente meu foco e minha paixão é por desenvolvimento front-end com JavaScript, React e seu vasto ecossistema.</p>
         </span>
       </div>
     </>
