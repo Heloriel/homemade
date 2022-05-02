bb6cbd60d9c0d26cf66df9bf9f6b5e37b51ee8f7
Heloriel <andradematheus.dev@gmail.com>
2022-05-02 11:00:01 -0300

    Logo link update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,66 +7,90 @@ import { AiOutlineMenu } from "react-icons/ai";
 import { CgClose } from "react-icons/cg";
 
 export default (props) => {
-	const [menuVisible, toggleMenu] = useState(false);
+  const [menuVisible, toggleMenu] = useState(false);
 
-	const toggleClass = () => {
-		toggleMenu(!menuVisible);
-	};
+  const toggleClass = () => {
+    toggleMenu(!menuVisible);
+  };
 
-	return (
-		<>
-			<nav className={"Navbar"} id="navbar">
-				<div className="col-6 Logo-Container p-0">
-					<span className="Logo" title="Imagine aqui uma logo maneira.">&#60;MeuPortfólio /&#62;</span>
-				</div>
-				<div className="col-6 Desktop-Nav d-none d-medium">
-					<ul>
-						<li key="dNav1">
-							<a role="button" href="#start" id="nav-start">INÍCIO</a>
-						</li>
-						<li key="dNav2">
-							<a role="button" href="#about" id="nav-about">SOBRE</a>
-						</li>
-						<li key="dNav3">
-							<a role="button" href="#skills" id="nav-skills">STACKS</a>
-						</li>
-						<li key="dNav4">
-							<a role="button" href="#projects" id="nav-projects">PROJETOS</a>
-						</li>
-						<li key="dNav5" title="Enviar E-Mail">
-							<a href="mailto:andradematheus.dev@gmail.com" className="btn btn-blue btn-contact">CONTATO</a>
-						</li>
-					</ul>
-				</div>
+  return (
+    <>
+      <nav className={"Navbar"} id="navbar">
+        <div className="col-6 Logo-Container p-0">
+          <a href="#start" id="nav-start" role="button">
+            <span className="Logo" title="Imagine aqui uma logo maneira.">
+              &#60;MeuPortfólio /&#62;
+            </span>
+          </a>
+        </div>
+        <div className="col-6 Desktop-Nav d-none d-medium">
+          <ul>
+            <li key="dNav1">
+              <a role="button" href="#start" id="nav-start">
+                INÍCIO
+              </a>
+            </li>
+            <li key="dNav2">
+              <a role="button" href="#about" id="nav-about">
+                SOBRE
+              </a>
+            </li>
+            <li key="dNav3">
+              <a role="button" href="#skills" id="nav-skills">
+                STACKS
+              </a>
+            </li>
+            <li key="dNav4">
+              <a role="button" href="#projects" id="nav-projects">
+                PROJETOS
+              </a>
+            </li>
+            <li key="dNav5" title="Enviar E-Mail">
+              <a href="mailto:andradematheus.dev@gmail.com" className="btn btn-blue btn-contact">
+                CONTATO
+              </a>
+            </li>
+          </ul>
+        </div>
 
-				{/* MOBILE MENU TOGGLE */}
-				<div className="col-6 Mobile-Nav-Toggle p-0 d-none d-mobile">
-					<a role="button" onClick={toggleClass}>
-						{menuVisible ? <CgClose /> : <AiOutlineMenu />}
-					</a>
-				</div>
-			</nav>
+        {/* MOBILE MENU TOGGLE */}
+        <div className="col-6 Mobile-Nav-Toggle p-0 d-none d-mobile">
+          <a role="button" onClick={toggleClass}>
+            {menuVisible ? <CgClose /> : <AiOutlineMenu />}
+          </a>
+        </div>
+      </nav>
 
-			{/* MOBILE MENU */}
-			<div className={menuVisible ? "Mobile-Nav-Menu" : "Mobile-Nav-Menu-Closed"}>
-				<ul>
-					<li key="mNav1">
-						<a role="button" href="#start" onClick={() => toggleMenu(false)}>INÍCIO</a>
-					</li>
-					<li key="mNav2">
-						<a role="button" href="#about" onClick={() => toggleMenu(false)}>SOBRE</a>
-					</li >
-					<li key="mNav3">
-						<a role="button" href="#skills" onClick={() => toggleMenu(false)}>STACKS</a>
-					</li>
-					<li key="mNav4">
-						<a role="button" href="#projects" onClick={() => toggleMenu(false)}>PROJETOS</a>
-					</li>
-					<li key="mNav5" title="Enviar E-Mail">
-						<a href="mailto:andradematheus.dev@gmail.com" onClick={() => toggleMenu(false)}>CONTATO</a>
-					</li>
-				</ul>
-			</div>
-		</>
-	);
+      {/* MOBILE MENU */}
+      <div className={menuVisible ? "Mobile-Nav-Menu" : "Mobile-Nav-Menu-Closed"}>
+        <ul>
+          <li key="mNav1">
+            <a role="button" href="#start" onClick={() => toggleMenu(false)}>
+              INÍCIO
+            </a>
+          </li>
+          <li key="mNav2">
+            <a role="button" href="#about" onClick={() => toggleMenu(false)}>
+              SOBRE
+            </a>
+          </li>
+          <li key="mNav3">
+            <a role="button" href="#skills" onClick={() => toggleMenu(false)}>
+              STACKS
+            </a>
+          </li>
+          <li key="mNav4">
+            <a role="button" href="#projects" onClick={() => toggleMenu(false)}>
+              PROJETOS
+            </a>
+          </li>
+          <li key="mNav5" title="Enviar E-Mail">
+            <a href="mailto:andradematheus.dev@gmail.com" onClick={() => toggleMenu(false)}>
+              CONTATO
+            </a>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 };
